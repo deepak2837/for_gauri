5e0a4d274f000407d237517215622ccef6dcd0fc
Deepak <52418151+deepak2837@users.noreply.github.com>
2023-04-24 17:56:25 +0530

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,335 @@
+import "./App.css";
+import * as React from "react";
+import PropTypes from "prop-types";
+
+import Box from "@mui/material/Box";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
+import TableSortLabel from "@mui/material/TableSortLabel";
+
+import Paper from "@mui/material/Paper";
+import axios from "axios";
+
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+
+import { visuallyHidden } from "@mui/utils";
+ 
+function descendingComparator(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+function getComparator(order, orderBy) {
+  return order === "desc"
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+function stableSort(array, comparator) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) {
+      return order;
+    }
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map((el) => el[0]);
+}
+
+const headCells = [
+  {
+    id: "first_name",
+    numeric: false,
+    disablePadding: true,
+    label: "first name",
+  },
+  {
+    id: "last_name",
+    numeric: false,
+    disablePadding: true,
+    label: "last name",
+  },
+  {
+    id: "email",
+    numeric: false,
+    disablePadding: false,
+    label: "email",
+  },
+  {
+    id: "gender",
+    numeric: false,
+    disablePadding: false,
+    label: "gender",
+  },
+  {
+    id: "income",
+    numeric: false,
+    disablePadding: false,
+    label: "income",
+  },
+  {
+    id: "car",
+    numeric: false,
+    disablePadding: false,
+    label: "car",
+  },
+  {
+    id: "phone_price",
+    numeric: false,
+    disablePadding: false,
+    label: "phone price",
+  },
+  {
+    id: "quote length",
+    numeric: false,
+    disablePadding: false,
+    label: "quote length",
+  },
+];
+
+const DEFAULT_ORDER = "asc";
+const DEFAULT_ORDER_BY = "calories";
+const DEFAULT_ROWS_PER_PAGE = 5;
+function EnhancedTableHead(props) {
+  const { order, orderBy, onRequestSort } = props;
+  const createSortHandler = (newOrderBy) => (event) => {
+    onRequestSort(event, newOrderBy);
+  };
+
+  return (
+    <TableHead>
+      <TableRow>
+        <TableCell padding="checkbox"></TableCell>
+        {headCells.map((headCell) => (
+          <TableCell
+            key={headCell.id}
+            align={headCell.numeric ? "right" : "left"}
+            padding={headCell.disablePadding ? "none" : "normal"}
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : "asc"}
+              onClick={createSortHandler(headCell.id)}
+            >
+              {headCell.label}
+              {orderBy === headCell.id ? (
+                <Box component="span" sx={visuallyHidden}>
+                  {order === "desc" ? "sorted descending" : "sorted ascending"}
+                </Box>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}
+
+EnhancedTableHead.propTypes = {
+  onRequestSort: PropTypes.func.isRequired,
+
+  order: PropTypes.oneOf(["asc", "desc"]).isRequired,
+  orderBy: PropTypes.string.isRequired,
+  rowCount: PropTypes.number.isRequired,
+};
+
+function App() {
+  const [order, setOrder] = React.useState(DEFAULT_ORDER);
+  const [orderBy, setOrderBy] = React.useState(DEFAULT_ORDER_BY);
+
+  const [page, setPage] = React.useState(0);
+  const [Data, setData] = React.useState([]);
+  const [visibleRows, setVisibleRows] = React.useState(null);
+  const [rowsPerPage, setRowsPerPage] = React.useState(DEFAULT_ROWS_PER_PAGE);
+  const [paddingHeight, setPaddingHeight] = React.useState(0);
+const rows = Data;
+console.log(rows)
+
+  // function to call the API and set the data state
+  const fetchData = async (filter) => {
+    console.log("value getting", filter);
+    try {
+      const response = await axios.get(
+        `http://127.0.0.1:6003/api/users?criteria=${filter}`
+      );
+      
+      setData(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+ 
+  React.useEffect(() => {
+    console.log("hi")
+    let rowsOnMount = stableSort(
+      rows,
+      getComparator(DEFAULT_ORDER, DEFAULT_ORDER_BY)
+    );
+
+    rowsOnMount = rowsOnMount.slice(
+      0 * DEFAULT_ROWS_PER_PAGE,
+      0 * DEFAULT_ROWS_PER_PAGE + DEFAULT_ROWS_PER_PAGE
+    );
+
+    setVisibleRows(rowsOnMount);
+  }, [rows]);
+
+  const handleRequestSort = React.useCallback(
+    (event, newOrderBy) => {
+      const isAsc = orderBy === newOrderBy && order === "asc";
+      const toggledOrder = isAsc ? "desc" : "asc";
+      setOrder(toggledOrder);
+      setOrderBy(newOrderBy);
+
+      const sortedRows = stableSort(
+        rows,
+        getComparator(toggledOrder, newOrderBy)
+      );
+      const updatedRows = sortedRows.slice(
+        page * rowsPerPage,
+        page * rowsPerPage + rowsPerPage
+      );
+
+      setVisibleRows(updatedRows);
+    },
+    [order, orderBy, page, rowsPerPage,rows]
+  );
+
+  const handleChangePage = React.useCallback(
+    (event, newPage) => {
+      setPage(newPage);
+
+      const sortedRows = stableSort(rows, getComparator(order, orderBy));
+      const updatedRows = sortedRows.slice(
+        newPage * rowsPerPage,
+        newPage * rowsPerPage + rowsPerPage
+      );
+
+      setVisibleRows(updatedRows);
+
+      // Avoid a layout jump when reaching the last page with empty rows.
+    },
+    [order, orderBy, rowsPerPage,rows]
+  );
+
+  const handleChangeRowsPerPage = React.useCallback(
+    (event) => {
+      const updatedRowsPerPage = parseInt(event.target.value, 10);
+      setRowsPerPage(updatedRowsPerPage);
+
+      setPage(0);
+
+      const sortedRows = stableSort(rows, getComparator(order, orderBy));
+      const updatedRows = sortedRows.slice(
+        0 * updatedRowsPerPage,
+        0 * updatedRowsPerPage + updatedRowsPerPage
+      );
+
+      setVisibleRows(updatedRows);
+
+      // There is no layout jump to handle on the first page.
+      setPaddingHeight(0);
+    },
+    [order,rows, orderBy]
+  );
+
+  return (
+    <div className="App">
+      <div>
+        <Stack direction="row" spacing={2}>
+          <Button variant="contained" onClick={() => fetchData(1)}>
+            income-lower-than-5-and-car-bmw-or-mercedes
+          </Button>
+          <Button variant="contained" onClick={() => fetchData(2)}>
+            male-users-with-phone-price-greater-than-10000
+          </Button>
+          <Button variant="contained" onClick={() => fetchData(3)}>
+            last-name-starts-with-m-and-quote-length-greater-than-15
+          </Button>
+          <Button variant="contained" onClick={() => fetchData(4)}>
+            car-bmw-mercedes-audi-email-does-not-include-digit
+          </Button>
+          <Button variant="contained" onClick={() => fetchData(5)}>
+            top-10-cities-with-highest-number-of-users-and-average-income
+          </Button>
+        </Stack>
+
+      
+      </div>
+
+      <Box sx={{ width: "100%" }}>
+        <Paper sx={{ width: "100%", mb: 2 }}>
+          <TableContainer>
+            <Table sx={{ minWidth: 750 }} aria-labelledby="tableTitle">
+              <EnhancedTableHead
+                order={order}
+                onRequestSort={handleRequestSort}
+                rowCount={rows.length}
+              />
+              <TableBody>
+                {visibleRows
+                  ? visibleRows.map((row, index) => {
+                      const labelId = `enhanced-table-checkbox-${index}`;
+
+                      return (
+                        <TableRow>
+                          <TableCell></TableCell>
+                          <TableCell
+                            component="th"
+                            id={labelId}
+                            scope="row"
+                            padding="none"
+                          >
+                            {row.first_name}
+                          </TableCell>
+                          <TableCell align="left">{row.last_name}</TableCell>
+                          <TableCell align="left">{row.email}</TableCell>
+                          <TableCell align="left">{row.gender}</TableCell>
+                          <TableCell align="left">{row.income}</TableCell>
+                          <TableCell align="left">{row.car}</TableCell>
+                          <TableCell align="left">{row.phone_price}</TableCell>
+                          <TableCell align="left">{row.quote}</TableCell>
+                        </TableRow>
+                      );
+                    })
+                  : null}
+                {paddingHeight > 0 && (
+                  <TableRow
+                    style={{
+                      height: paddingHeight,
+                    }}
+                  >
+                    <TableCell colSpan={6} />
+                  </TableRow>
+                )}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <TablePagination
+            rowsPerPageOptions={[5, 10, 25]}
+            component="div"
+            count={rows.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+          />
+        </Paper>
+      </Box>
+    </div>
+  );
+}
+export default App;
